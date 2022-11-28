ce84bfda8c7db5fbcb187f2ecc4292c58018a41b
Mark Che <screenfriskofficial@gmail.com>
2022-11-28 22:25:27 +1100

    Фикшу баги

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import Test from './components/pages/Test';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Header from './components/Header';
-import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
-      <Analytics />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
